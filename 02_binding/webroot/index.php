6b870e2aed7327b3429c22396f679578701081ce
pozda2 <pozda2@users.noreply.github.com>
2023-11-23 12:25:28 +0100

    Lekce 02

---
+++ 02_binding/webroot/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Docker - hello world</title>
+	<style type="text/css">
+	body {
+		background-color: lightsteelblue;
+	}
+	#main-content {
+		margin: 100px;
+		text-align: center;
+	}
+	.tenor-gif-embed {
+		width: 400px !important;
+		margin: 0 auto;
+	}
+</style>
+</head>
+<body>
+	<div id="main-content">
+		<h1>
+			Hello world
+		</h1>
+	</div>
+</body>
+</html>
